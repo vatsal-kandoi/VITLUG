703f9e8eaf0cca9cb78d8a9a2a22450eb9acaaaa
Vatsal Kandoi <vatsalkandoi1998@gmail.com>
2019-12-03 00:15:26 +0530

    Added admin dashboard

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -24,3 +24,40 @@ document.getElementById('admin-signin').addEventListener('submit', (e)=> {
         password: document.getElementById('password').value
     }));
 });
+
+document.getElementById('quizzes').addEventListener('click', () => {
+    let xhttp = new XMLHttpRequest();
+    xhttp.open('GET','/api/admin/quiz');
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4) {
+            if (xhttp.status == 200) {
+                try {
+                    let x = JSON.parse(xhttp.responseText);
+                    if (x.code == 200) {
+                        document.getElementById('quiz-view').innerHTML = '';
+                        document.getElementById('user-view').innerHTML = '';
+                        for (let i=0;i<x.responses;i++) {
+                            document.getElementById('quiz-view').innerHTML+=`
+                                <div onclick="getQuizByID(${x.responses[i]._id})">
+                                    <div class="user">
+                                        <div class="name">${x.responses[i].user.name}</div>
+                                        <div class="regno">${x.responses[i].user.regno}</div>
+                                    </div>                                
+                                </div>
+                            `;
+                        }
+                    } else {
+                        alert('Error');
+                    }
+                } catch (err) {
+
+                }
+            }
+        } 
+    }
+    xhttp.send();
+});
+
+function getQuizByID(quizId) {
+    window.location.href = `/admin/dashboard/quiz?quizId=${quizId}`;
+};
